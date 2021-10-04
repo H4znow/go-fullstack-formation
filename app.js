33ed0d17c0b62d30ed606ee026e3f471cfac60ea
Amine Haddou <haddouamine2002@gmail.com>
2021-10-04 12:47:49 +0300

    Base application et server

---
+++ app.js
@@ -0,0 +1,24 @@
+    const express = require('express');
+
+    const app = express();
+
+    app.use((req, res, next) => {
+        console.log("Requete recue !");
+        next();
+    });
+
+    app.use((req, res, next) =>{
+        res.status(201);
+        next();
+    });
+
+    app.use((req, res, next) => {
+        res.json({message: 'Votre requete a bien ete recu'});
+        next();
+    });
+
+    app.use((req, res) => {
+        console.log('Reponse envoyee avec succes !');
+    });
+
+    module.exports = app;
