7cfc2e97c83ff6346b36a0cbeb5f5645b7491237
Amine Haddou <haddouamine2002@gmail.com>
2021-10-05 19:59:07 +0300

    API fini avec 4  ROUTES [PUT, GET, DELETE, POST]

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ mongoose.connect('mongodb+srv://Haznow:yTblFrQ9zuR8K5U7@cluster0.tedyh.mongodb.n
 .then(() => console.log('Connexion à MongoDB réussie !'))
 .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+//CROS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -17,6 +18,7 @@ app.use((req, res, next) => {
     next();
 });
 
+//Lire le body de req
 app.use(bodyParser.json());
 
 app.post('/api/stuff', (req, res, next) => {
@@ -29,9 +31,29 @@ app.post('/api/stuff', (req, res, next) => {
     .catch(error => res.status(400).json({ error }));
 });
 
-app.use('/api/stuff', (req, res, next) => {
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, {...req.body, _id : req.params.id})
+    .then(()=> res.status(200).json({message: 'Objet modifie !'}))
+    .catch(error => res.status(400).json({ error }));
+
+});
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({ _id : req.params.id })
+    .then(()=> res.status(200).json({ message : "Objet supprimes !" }))
+    .catch( error => res.status(400).json({ error }));
+});
+
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({ _id : req.params.id })
+    .then( thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+});
+
+app.get('/api/stuff', (req, res, next) => {
   Thing.find()
-    .then(things => res.status(200).json(things))
+    .then(thing => res.status(200).json(thing))
     .catch(error => res.status(400).json({ error }));
 });
+
 module.exports = app;
